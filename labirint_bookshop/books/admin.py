be542c5716002e4b95952051e95181b9572bebc8
PyPie0 <ilyaVoronco.v@yandex.ru>
2022-08-09 22:26:16 +0300

    Register models in admin sites
    Register models in admin sites and fix debug urls

--- labirint_bookshop/books/admin.py
+++ labirint_bookshop/books/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
+from .models import (
+	Category,
+	Author,
+	Translator,
+	Illustrator,
+	Book,
+	PublishingHouse,
+)
 
-# Register your models here.
+
+class PublishingHouseAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
+
+class AuthorAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('first_name', 'last_name')}
+
+
+class TranslatorAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('first_name', 'last_name')}
+
+
+class IllustratorAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('first_name', 'last_name')}
+
+
+class CategoryAdmin(DraggableMPTTAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
+
+class BookAdmin(admin.ModelAdmin):
+	pass
+
+
+admin.site.register(PublishingHouse, PublishingHouseAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Translator, TranslatorAdmin)
+admin.site.register(Illustrator, IllustratorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
